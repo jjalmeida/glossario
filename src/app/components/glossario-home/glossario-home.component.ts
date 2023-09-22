1065f8582dfd84a2213d665125dde01f53ed92b2
Junior <jose.junior@fujitsu.com>
2023-09-22 17:00:01 -0300

    configurando metodo de recuperacao de verbetes no modulo glossasrio-home

--- src/app/components/glossario-home/glossario-home.component.ts
+++ src/app/components/glossario-home/glossario-home.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { VerbeteService } from 'src/app/services/verbete.service';
+import { Subject, takeUntil } from 'rxjs';
+import { VerbeteResponseDTO } from 'src/app/models/interfaces/response/VerbeteResponseDTO';
 
 @Component({
   selector: 'app-glossario-home',
   templateUrl: './glossario-home.component.html',
   styleUrls: []
 })
-export class GlossarioHomeComponent {
+export class GlossarioHomeComponent implements OnInit, OnDestroy{
+  private readonly destroy$: Subject<void> = new Subject();
+  public verbetesDatas: Array<VerbeteResponseDTO> = [];
 
+  constructor(
+    private verbeteService: VerbeteService,
+    private messageService: MessageService){}
+
+  ngOnInit(): void {
+    this.getServiceVerbetesDatas();
+  }
+
+  getServiceVerbetesDatas() {
+    this.verbeteService
+      .getAllVerbetes()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0){
+            this.verbetesDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar lista de verbetes',
+            life: 2500
+          })
+        }
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- src/app/services/verbete.service.ts
+++ src/app/services/verbete.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { enviroment } from 'src/app/enviroments/enviroment';
+import { VerbeteResponseDTO } from '../models/interfaces/response/VerbeteResponseDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,10 @@ export class VerbeteService {
   private API_URL = enviroment.API_URL;
 
   constructor(private httpClient: HttpClient) { }
+
+  getAllVerbetes(): Observable<Array<VerbeteResponseDTO>>{
+    return this.httpClient.get<Array<VerbeteResponseDTO>>(
+      `${this.API_URL}`
+    );
+  }
 }
