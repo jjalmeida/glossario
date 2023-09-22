be065025a73e9652d663e4b43100da3c7ac13219
Junior <jose.junior@fujitsu.com>
2023-09-22 16:50:06 -0300

    criacao dos arquivos de variaveis de ambiente

--- src/app/services/verbete.service.ts
+++ src/app/services/verbete.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { enviroment } from 'src/app/enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerbeteService {
 
-  constructor() { }
+  private API_URL = enviroment.API_URL;
+
+  constructor(private httpClient: HttpClient) { }
 }
