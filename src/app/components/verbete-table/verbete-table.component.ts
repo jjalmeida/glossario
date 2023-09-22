9081adb0328822eb71dbbaeaca91bbd607d1bf74
Junior <jose.junior@fujitsu.com>
2023-09-22 17:10:28 -0300

    configurando apresentacao dos verbetes no componente verbate-table

--- src/app/components/verbete-table/verbete-table.component.ts
+++ src/app/components/verbete-table/verbete-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { VerbeteResponseDTO } from 'src/app/models/interfaces/response/VerbeteResponseDTO';
 
 @Component({
   selector: 'app-verbete-table',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class VerbeteTableComponent {
-
+  @Input() verbetes: Array<VerbeteResponseDTO> = [];
 }
